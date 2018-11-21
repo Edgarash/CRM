1a535795a06543983516c4315b1148f99cd49a3b
Edgar Michell Cisneros García <edgar.31896@gmail.com>
2018-11-21 00:17:01 -0700

    Fpdf

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,28 @@ Route::get('/', function () {
 
 
 //Maribel Montes
+// use Anouar\Fpdf\Facades\Fpdf;
 
-
+Route::get('/nel', function() {
+	$pdf = new Fpdf();
+	$pdf::AddPage();
+	$pdf::SetFont('Arial','B',18);
+	$pdf::Cell(0,10,"Title",0,"","C");
+	$pdf::Ln();
+	$pdf::Ln();
+	$pdf::SetFont('Arial','B',12);
+	$pdf::cell(25,8,"ID",1,"","C");
+	$pdf::cell(45,8,"Name",1,"","L");
+	$pdf::cell(35,8,"Address",1,"","L");
+	$pdf::Ln();
+	$pdf::SetFont("Arial","",10);
+	$pdf::cell(25,8,"1",1,"","C");
+	$pdf::cell(45,8,"John",1,"","L");
+	$pdf::cell(35,8,"New York",1,"","L");
+	$pdf::Ln();
+	$pdf::Output();
+	exit;
+});
 
 //Adalberto Palafox
 
