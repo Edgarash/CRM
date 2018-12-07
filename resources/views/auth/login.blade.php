900b6579c6ef807414b2e7ab73671423d7e20b1e
mgosuna95 <mgosuna95@gmail.com>
2018-12-07 01:55:45 -0700

    mas

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -98,35 +98,7 @@
                             <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                             <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
                         </ol>
-                        <div class="carousel-inner" role="listbox">
-                            <div class="carousel-item active">
-                                <img class="d-block img-fluid" src="https://static.pexels.com/photos/33972/pexels-photo.jpg" alt="First slide">
-                                <div class="carousel-caption d-none d-md-block">
-                                    <div class="banner-text">
-                                        <h2>Nuevas Soluciones</h2>
-                                        <p>Microsistemas Californianos</p>
-                                    </div>	
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img class="d-block img-fluid" src="https://images.pexels.com/photos/7097/people-coffee-tea-meeting.jpg" alt="First slide">
-                                <div class="carousel-caption d-none d-md-block">
-                                    <div class="banner-text">
-                                        <h2>Nuevos Equipos</h2>
-                                        <p>Microsistemas Californianos</p>
-                                    </div>	
-                                </div>
-                            </div>
-                            <div class="carousel-item">
-                                <img class="d-block img-fluid" src="https://images.pexels.com/photos/872957/pexels-photo-872957.jpeg" alt="First slide">
-                                <div class="carousel-caption d-none d-md-block">
-                                    <div class="banner-text">
-                                        <h2>Un nuevo paradigma</h2>
-                                        <p>Microsistemas Californianos</p>
-                                    </div>	
-                                </div>
-                            </div>
-                        </div>	   
+                        
                     </div>
                 </div>
             </div>  
